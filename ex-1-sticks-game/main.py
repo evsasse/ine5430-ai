7cb8cec953121d418793adf867a4614b1a854662
Evandro Sasse <evsasse@gmail.com>
2017-04-11 23:46:39 -0300

    game working

--- ex-1-sticks-game/main.py
+++ ex-1-sticks-game/main.py
@@ -5,54 +5,88 @@ MAX_PLAYERS = 5
 INITIAL_STICKS = 3
 
 class Player:
-    def __init__(self, name, players):
+    def __init__(self, name, players_names):
         self.name = name
         self.sticks = INITIAL_STICKS
 
         self.other_players_sticks = {}
-        for player in players:
-            if player == name:  continue
-            self.other_players_sticks[player] = INITIAL_STICKS
+        for player_name in players_names:
+            if player_name == name:  continue
+            self.other_players_sticks[player_name] = INITIAL_STICKS
 
-    # def fix_knowledge_winner(self, winner)
+    def update_winner_sticks(self, winner_name):
+        if self.name == winner_name:
+            self.sticks -= 1
+        else:
+            self.other_players_sticks[winner_name] -= 1
+
+    def can_win(self):
+        return self.sticks == 1
 
     def __guess(self, at_least_per_player):
         at_least = self.holding + len(self.other_players_sticks) * at_least_per_player
         # print("= Player '{}' would guess at least {}, because {}".format(self.name, at_least, [self.holding, len(self.other_players_sticks) * at_least_per_player]))
         return randint(at_least, sum(self.other_players_sticks.values()))
 
-    def guess(self, other_guesses, at_least_per_player = 0):
+    def guess(self, other_guesses, at_least_per_player):
         to_guess = self.__guess(at_least_per_player)
         while to_guess in other_guesses:
             to_guess = self.__guess(at_least_per_player)
         # print("= Player '{}' is guessing {}, because {}".format(self.name, to_guess, [other_guesses, at_least_per_player]))
         return to_guess
 
-    def hold(self, at_least = 0):
+    def hold(self, at_least):
         self.holding = randint(at_least, self.sticks)
         return self.holding
 
 def __main__():
-    players = []
-    players_names = list(range(0, MAX_PLAYERS))
+    players = {}
+    players_names = ['P1', 'P2', 'P3', 'P4', 'P5']
     for player_name in players_names:
-        players.append(Player(player_name, players_names))
+        players[player_name] = Player(player_name, players_names)
 
     winner = None
     round_number = 0
-    # while(winner is None):
-    if True:
-        print(' === Starting round #{} === '.format(round_number))
 
+    while winner is None:
+    # if True:
+        print('=== Starting round #{} === '.format(round_number))
+        print(' = Player {} is going to be the first to guess'.format(players_names[0]))
+
+        # cant hold 0 on first round
         at_least_per_player = 1 if round_number == 0 else 0
 
-        round_holds = [player.hold(at_least_per_player) for player in players]
-        print(' = Shh! = Players have holded: {}, for a total of: {}'.format(round_holds, sum(round_holds)))
+        round_holds = [player.hold(at_least_per_player) for player in players.values()]
+        print(' = Shh! = Players are holding: {}, for a total of {}'.format(round_holds, sum(round_holds)))
 
         round_guesses = []
-        for player in players:
-            round_guesses.append(player.guess(round_guesses, at_least_per_player))
+        round_winner = None
+        for player_name in players_names:
+            player = players[player_name]
+            player_guess = player.guess(round_guesses, at_least_per_player)
+            round_guesses.append(player_guess)
+            if player_guess == sum(round_holds):
+                round_winner = player
 
-        print(' = Players have guessed {}'.format(round_guesses))
+        print(' = Players have guessed: {}'.format(round_guesses))
+
+        rotate_players_by = 1
+        if round_winner is not None:
+            if round_winner.can_win():
+                winner = round_winner
+                print(' = Player {} won the round guessing {}, and won the game'.format(round_winner.name, sum(round_holds)))
+                print('=== GAME ENDED ===')
+            else:
+                for player in players.values(): player.update_winner_sticks(round_winner.name)
+                print(' = Player {} won the round guessing {}, and has one fewer stick'.format(round_winner.name, sum(round_holds)))
+                print(' = Shh! = Player {} has {} sticks left'.format(round_winner.name, round_winner.sticks))
+                rotate_players_by = players_names.index(round_winner.name)
+        else:
+            print(' = No players have guessed correctly this round :(')
+
+        # rotates order to be guessed by one, or to the round_winner to be first
+        players_names = players_names[rotate_players_by:]+players_names[:rotate_players_by]
+
+        round_number += 1
 
 __main__()
